468febca426f91b06f77951e1ea461561f10e1c7
Closer94 <kabsung3@naver.com>
2022-06-23 15:20:48 +0900

    refactor: jwt를 재생성시 Cookie에 jwt값이 있는지 확인하는 소스 추가

--- src/main/java/com/example/demo/security/rememberMe/RememberMeCheckFilter.java
+++ src/main/java/com/example/demo/security/rememberMe/RememberMeCheckFilter.java
@@ -5,6 +5,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -15,7 +16,7 @@ import java.util.Base64.Decoder;
 import java.util.List;
 
 @Component
-public class RememberMeCheckFilter implements Filter{
+public class RememberMeCheckFilter implements Filter {
 
     Authentication authentication;
 
@@ -29,33 +30,38 @@ public class RememberMeCheckFilter implements Filter{
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         String userId = null;
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
 
-        System.out.println("###################### RememberMe Check Filter 입니다 ########################################################################");
-        String rememberMeCookie = extractRememberMeCookie((HttpServletRequest) request);
-        
-        //쿠키에 remeberMe 가 있다면 token 값 새로 발급
-        if(rememberMeCookie != null) {
-            userId = extractUserId(rememberMeCookie);
-            System.out.println("################## 사용자 ID: " + userId);
-            insertTokenOnCookie(userId, (HttpServletResponse)response);
+        boolean isTokenInCookies = checkTokenInCookies(httpRequest);
+        String rememberMeCookie = extractRememberMeCookie(httpRequest);
+
+        //쿠키에 token이 있는지 없는지 확인
+        if (isTokenInCookies == false) {
+            //쿠키에 remeberMe 가 있다면 token 값 새로 발급
+            if (rememberMeCookie != null) {
+                userId = extractUserId(rememberMeCookie);
+                System.out.println("################## 사용자 ID: " + userId);
+                insertTokenOnCookie(userId, httpResponse);
+                httpResponse.sendRedirect("/welcome");
+            }
         }
-
         chain.doFilter(request, response);
     }
 
-    public void insertTokenOnCookie(String userId, HttpServletResponse response){
+    public void insertTokenOnCookie(String userId, HttpServletResponse httpResponse) {
         final String user = userId;
         final List<String> roles = new ArrayList<>();
         roles.add("ROLE_USER");
-        final String token = jwtTokenProvider.createToken(user,roles);
-        Cookie cookie = new Cookie("jwt",token);
+        final String token = jwtTokenProvider.createToken(user, roles);
+        Cookie cookie = new Cookie("jwt", token);
         cookie.setPath("/");
-        response.addHeader("Authorization", "BEARER"+ " " + token); // jwt 토큰을 헤더로 넘기고 싶으면!
-        response.addCookie(cookie); // jwt 토큰을 쿠키로 넘기고 싶으면!
+        httpResponse.addHeader("Authorization", "BEARER" + " " + token); // jwt 토큰을 헤더로 넘기고 싶으면!
+        httpResponse.addCookie(cookie); // jwt 토큰을 쿠키로 넘기고 싶으면!
     }
 
     //RememberMe 에 있는 사용자 ID 값 가져오기
-    public String extractUserId(String rememberMeCookie){
+    public String extractUserId(String rememberMeCookie) {
         String decodeResult = null;
         String[] rememberInfo = null;
 
@@ -66,12 +72,12 @@ public class RememberMeCheckFilter implements Filter{
         rememberInfo = decodeResult.split(":");
         return rememberInfo[0];
     }
-    
+
     //쿠키에 RememberMe 가져오기
-    public String extractRememberMeCookie(HttpServletRequest request){
+    public String extractRememberMeCookie(HttpServletRequest httpRequest) {
         String rememberMeCookie = null;
-        Cookie[] cookies=request.getCookies(); // 모든 쿠키 가져오기
-        if(cookies!=null){
+        Cookie[] cookies = httpRequest.getCookies(); // 모든 쿠키 가져오기
+        if (cookies != null) {
             for (Cookie c : cookies) {
                 String name = c.getName(); // 쿠키 이름 가져오기
                 String value = c.getValue(); // 쿠키 값 가져오기
@@ -83,5 +89,22 @@ public class RememberMeCheckFilter implements Filter{
         }
 
         return rememberMeCookie;
+    }
+
+    private boolean checkTokenInCookies(HttpServletRequest httpRequest) {
+        boolean tokenFlag = false;
+
+        Cookie[] cookies = httpRequest.getCookies(); // 모든 쿠키 가져오기
+        if (cookies != null) {
+            for (Cookie c : cookies) {
+                String name = c.getName(); // 쿠키 이름 가져오기
+                String value = c.getValue(); // 쿠키 값 가져오기
+                if (name.equals("jwt")) {
+                    tokenFlag = true;
+                }
+            }
+        }
+
+        return tokenFlag;
     }
 }
